0449015c267bb51e5517a0d8554bbb5ca71a8186
Johannes Langbein <jllangbein@gmail.com>
2025-07-22 12:06:56 +0200

    ColorScheme from seed teal

---
+++ lib/main_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
